c9095ecbcea27e41dec8870fb14ead429203d766
Andrei Moiceanu <andrei.moiceanu29@gmail.com>
2018-06-21 15:51:53 -0700

    Setup production build and server

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,23 +2,28 @@ import React from 'react'
 import selectExpenses from '../selectors/expenses'
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
-const ExpenseList= (props)=>(
+const ExpenseList = (props) => (
     <div>
-    <h1> Expense List</h1>
-    {props.expenses.map((expense)=>{
-      return <ExpenseListItem key={expense.id} {...expense}/>
-    })}
-    
+        <h1> Expense List</h1>
+        {props.expenses.length===0&&<p>No expenses</p>}
+         {
+        
+            props.expenses.map((expense) => {
+                return <ExpenseListItem key={expense.id} {...expense} />
+            })
+        
+        }
+
     </div>
 )
 
-const mapStateToProps=(state)=>{
-    return{
-        expenses:selectExpenses(state.expenses,state.filters)
+const mapStateToProps = (state) => {
+    return {
+        expenses: selectExpenses(state.expenses, state.filters)
     }
 }
 
-const ConnectedExpenseList=connect(mapStateToProps)(ExpenseList);
-      
+const ConnectedExpenseList = connect(mapStateToProps)(ExpenseList);
+
 
 export default ConnectedExpenseList;
